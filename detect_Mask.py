9860fd74c9eb9b570afd721c95871398d12fe79e
joskalenda <joskalenda3@gmail.com>
2021-09-15 15:54:46 +0200

    Creat detect function to predict the image

--- detect_Mask.py
+++ detect_Mask.py
@@ -7,3 +7,21 @@ import imutils
 import time
 import cv2
 import os
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+    # grab the dimensions of the frame and then construct a blob
+    # from it
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+        (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the face detections
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    # initialize our list of faces, their corresponding locations,
+    # and the list of predictions from our face mask network
+    faces = []
+    locs = []
+    preds = []
+
