a2b7bc10c29187a4c2e2dee199ab8861cc4c52f1
joskalenda <joskalenda3@gmail.com>
2021-09-17 14:42:10 +0200

    Creat the image frame

--- Gather_images.py
+++ Gather_images.py
@@ -30,3 +30,36 @@ cap = cv2.VideoCapture(0)
 
 start = False
 count = 0
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        continue
+
+    if count == num_samples:
+        break
+
+ #   cv2.rectangle(frame, (10, 30), (310, 330), (0, 255, 0), 2)
+
+    if start:
+      #  roi = frame[100:500, 100:500]
+        #time.sleep(1)
+        save_path = os.path.join(IMG_CLASS_PATH, '{}.jpg'.format(count + 1))
+        cv2.imwrite(save_path, frame)
+        count += 1
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, "Collecting {}".format(count),
+            (5, 10), font, 0.7, (0, 255, 255), 2, cv2.LINE_AA)
+    cv2.imshow("Collecting images", frame)
+
+    k = cv2.waitKey(10)
+    if k == ord('a'):
+        start = not start
+
+    if k == ord('q'):
+        break
+
+print("\n{} image(s) saved to {}".format(count, IMG_CLASS_PATH))
+cap.release()
+cv2.destroyAllWindows()
