6ef9d3219b83302f9701e53576a135077808eb0c
joskalenda <joskalenda3@gmail.com>
2021-09-17 14:57:55 +0200

    Initialize training parameters

--- training.py
+++ training.py
@@ -3,3 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.applications import MobileNetV2
 import os
+
+# initialize the initial learning rate, number of epochs to train for,
+# and batch size
+INIT_LR = 1e-4
+EPOCHS = 20
+BS = 32
+SD = 123
+width = 224
+height = 224
+data_dir = r"dataset"
+print("[INFO] loading images...")
