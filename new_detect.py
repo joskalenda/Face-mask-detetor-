b819107d6977948f9bf1e3f15510838dd0cfaba8
joskalenda <joskalenda3@gmail.com>
2021-09-18 22:15:48 +0200

    Process the image

--- new_detect.py
+++ new_detect.py
@@ -27,3 +27,9 @@ net = cv2.dnn.readNet(prototxtPath, weightsPath)
 # load the face mask detector model from disk
 print("[INFO] loading face mask detector model...")
 model = load_model(args["model"])
+mage = cv2.imread(args["image"])
+orig = image.copy()
+(h, w) = image.shape[:2]
+# construct a blob from the image
+blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
+    (104.0, 177.0, 123.0))
