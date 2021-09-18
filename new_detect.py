fd332ae262c835b3bc23cc7e12e089d8742b6e92
joskalenda <joskalenda3@gmail.com>
2021-09-18 22:12:32 +0200

    Define arguments parser

--- new_detect.py
+++ new_detect.py
@@ -5,3 +5,16 @@ import numpy as np
 import argparse
 import cv2
 import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "-new2.png", required=True,
+    help="path to input image")
+ap.add_argument("-f", "--face", type=str,
+    default="MaskDetector.model",
+    help="path to face detector model directory")
+ap.add_argument("-m", "--model", type=str,
+    default="MaskDetector.model",
+    help="path to trained face mask detector model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+    help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
