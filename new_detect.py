dd04d2fc26b75200883eb0533a0e1b6a93568379
joskalenda <joskalenda3@gmail.com>
2021-09-18 22:17:10 +0200

    Compute face detection

--- new_detect.py
+++ new_detect.py
@@ -32,4 +32,9 @@ orig = image.copy()
 (h, w) = image.shape[:2]
 # construct a blob from the image
 blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
-    (104.0, 177.0, 123.0))
+                            (104.0, 177.0, 123.0))
+
+# pass the blob through the network and obtain the face detections
+print("[INFO] computing face detections...")
+net.setInput(blob)
+detections = net.forward()
