eaad4f8fba35e9655b069d4d381307110db85e9a
joskalenda <joskalenda3@gmail.com>
2021-09-18 22:10:57 +0200

    Import libraries for training process

---
+++ new_detect.py
@@ -0,0 +1,7 @@
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import argparse
+import cv2
+import os
