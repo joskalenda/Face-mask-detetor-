7e7d56207bd7feb52271b01c7488281b6b15b10a
joskalenda <joskalenda3@gmail.com>
2021-09-18 22:06:27 +0200

    Processing the image and train the model

--- training.py
+++ training.py
@@ -35,3 +35,26 @@ validation = tf.keras.preprocessing.image_dataset_from_directory(
 
 classes = training.class_names
 classes
+
+
+for images, labels in training.take(1):
+	plt.imshow(images[1].numpy().astype('uint8'))
+	plt.title(classes[labels[1]])
+
+model = MobileNetV2(weights='imagenet')
+model.compile(optimizer='adam', 
+loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+metrics=['accuracy'])
+
+model.summary()
+face_mask_detection = model.fit(training, validation_data=validation,epochs=3)
+
+img = tf.keras.preprocessing.image.load_img('new2.png', target_size=(height, width))
+image_array = tf.keras.preprocessing.image.img_to_array(img)
+image_array = tf.expand_dims(image_array, 0)
+image_array.shape
+
+predictions = model.predict(image_array)
+score = tf.nn.softmax(predictions[0])
+
+print(score)
